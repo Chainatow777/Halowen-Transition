e5dbb3fcb43090afef1f57e35f03bab22909579a
Samuell Morgesntern de Souza <107222555+Chainatow777@users.noreply.github.com>
2023-06-29 16:08:27 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// header
-
-const btnMobile = document.getElementById('btn-mobile')
-
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault()
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active')
-}
-
-// header
-
 // menu
 
 function buttonClick() {
@@ -21,27 +9,3 @@ function buttonClick() {
         splash.remove();
       }, 1000);
 }
-
-// card
-
-btnMobile.addEventListener('click', toggleMenu)
-btnMobile.addEventListener('touchstart', toggleMenu)
-
-var buttons = document.getElementsByClassName('read_button')
-
-for (var i = 0; i < buttons.length; i ++) { 
-
-buttons[i].addEventListener('click', function(evt) {
-    var card = evt.currentTarget.parentElement
-    card.classList.toggle('active')
-
-    if (card.classList.contains('active')) {
-        return evt.currentTarget.textContent = 'Leia Menos'
-    }
-
-    evt.currentTarget.textContent = 'Leia Mais'
-})
-
-}
-
-// card
